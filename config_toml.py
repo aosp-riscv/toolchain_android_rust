04e2f87e0ff2def78cd2b256b6b90879cd8e4835
Jeff Vander Stoep <jeffv@google.com>
2020-07-22 17:00:55 +0200

    Keep thrashing until something builds
    Change-Id: Ia4eaffbab99e978becdf91749cc866176edc7747

--- config_toml.py
+++ config_toml.py
@@ -34,6 +34,7 @@ def configure():
         cc = paths.llvm_prebuilt('bin', 'clang')
         cxx = paths.llvm_prebuilt('bin', 'clang++')
         ar = paths.llvm_prebuilt('bin', 'llvm-ar')
+        cxxstd = paths.llvm_prebuilt('lib64', "clang", "11.0.1")
         ranlib = paths.llvm_prebuilt('bin', 'llvm-ranlib')
 
         def host_config(target):
@@ -61,9 +62,9 @@ def configure():
 
             with open(cxx_wrapper_name, 'w') as f:
                 f.write("""\
-#!/bin/sh
-{real_cxx} $* --target={target} {sysroot_flags}
-""".format(real_cxx=cxx, target=target, sysroot_flags=sysroot_flags))
+#!/bin/bash -v
+{real_cxx} -I{cxxstd} $* --target={target} {sysroot_flags}
+""".format(real_cxx=cxx, target=target, sysroot_flags=sysroot_flags, cxxstd=cxxstd))
 
             s = os.stat(wrapper_name)
             os.chmod(wrapper_name, s.st_mode | stat.S_IEXEC)
