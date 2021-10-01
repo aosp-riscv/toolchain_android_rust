30f44a2ad58b45884fb7a9c741f17877b28e84ac
Chris Wailes <chriswailes@google.com>
2021-10-01 15:54:16 -0700

    Add an option to enable LTO when compiling rustc
    This CL adds an LTO option to do_build.py.  This option will enable
    either Full or Think LTO when building rustc depending on the argument
    provided.
    
    Test: ./toolchain/android_rust/build.py
    Test: ./toolchain/android_rust/build.py --lto full
    Test: ./toolchain/android_rust/build.py --lto thin
    Change-Id: Ia705fb711a25da75f7ec22d811dc49148630522e

--- config.py
+++ config.py
@@ -13,6 +13,8 @@
 # limitations under the License.
 """Handles generation of config.toml for the rustc build."""
 
+import argparse
+import os
 import subprocess
 import stat
 from string import Template
@@ -38,6 +40,7 @@ HOST_CXX_WRAPPER_TEMPLATE:      Path = TEMPLATES_PATH / 'host_cxx_wrapper.templa
 HOST_LINKER_WRAPPER_TEMPLATE:   Path = TEMPLATES_PATH / 'host_linker_wrapper.template'
 HOST_TARGET_TEMPLATE:           Path = TEMPLATES_PATH / 'host_target.template'
 
+LINKER_PIC_FLAG:     str = '-Wl,-mllvm,-relocation-model=pic'
 MACOSX_VERSION_FLAG: str = '-mmacosx-version-min=10.14'
 
 
@@ -119,8 +122,12 @@ def device_config(target: str) -> str:
             ar=AR_PATH)
 
 
-def configure():
+def configure(args: argparse.ArgumentParser, env: dict[str, str]):
     """Generates config.toml and compiler wrapers for the rustc build."""
+
+    #
+    # Compute compiler/linker flags
+    #
 
     macosx_flags:          str = ''
     host_ld_selector:      str = '-fuse-ld=lld' if build_platform.is_linux() else ''
@@ -129,6 +136,12 @@ def configure():
     host_rpath_buildtime:  str = '-Wl,-rpath,' + LLVM_CXX_RUNTIME_PATH.as_posix()
     host_rpath_runtime:    str = '-Wl,-rpath,' + (
         '$ORIGIN/../lib64' if build_platform.is_linux() else '@loader_path/../lib64')
+
+    lto_flag: str = ''
+    if args.lto == 'full':
+        lto_flag = '-flto=full'
+    elif args.lto == 'thin':
+        lto_flag - '-flto=thin'
 
     if build_platform.is_darwin():
         # Apple removed the normal sysroot at / on Mojave+, so we need
@@ -142,10 +155,65 @@ def configure():
 
     host_linker_flags = ' '.join([
         host_ld_selector,
+        LINKER_PIC_FLAG,
+        lto_flag,
         host_bin_search,
         host_llvm_libpath,
         host_rpath_buildtime,
         host_rpath_runtime])
+
+    #
+    # Update environment variables
+    #
+
+    env['PATH'] = os.pathsep.join(
+        [p.as_posix() for p in [
+          RUST_PREBUILT_PATH / 'bin',
+          CMAKE_PREBUILT_PATH / 'bin',
+          NINJA_PREBUILT_PATH,
+          BUILD_TOOLS_PREBUILT_PATH,
+        ]] + [env['PATH']])
+
+    # Only adjust the library path on Linux - on OSX, use the devtools curl
+    if build_platform.is_linux():
+        if 'LIBRARY_PATH' in env:
+            old_library_path = ':{0}'.format(env['LIBRARY_PATH'])
+        else:
+            old_library_path = ''
+        env['LIBRARY_PATH'] = '{0}{1}'.format(CURL_PREBUILT_PATH / 'lib', old_library_path)
+
+    # Tell the rust bootstrap system where to place its final products
+    env['DESTDIR'] = OUT_PATH_PACKAGE
+
+    # Pass additional flags to the Rust compiler
+    env['RUSTFLAGS'] = '-C relocation-model=pic'
+
+    # The LTO flag must be passed via the HOST_CFLAGS environment variable due
+    # to the fact that including it in the host c/cxx wrappers will cause the
+    # CMake compiler detection routine to fail during LLVM configuration.
+    #
+    # The Rust bootstrap system will include the value of HOST_CFLAGS in all
+    # invocations of either the C or C++ compiler for host targets.  The LLVM
+    # build system will receive this value from the llvm::cflags and
+    # llvm::cxxflags flags in the config.toml file instantiated below.
+    #
+    # Because Rust's bootstrap system doesn't pass the linker wrapper into the
+    # LLVM build system AND doesn't respect the LDFLAGS environment variable
+    # this value gets into the LLVM build system via the llvm::ldflags value in
+    # config.toml and the Rust build system via the host linker wrapper, both
+    # of which are instantiated below using the same value for host_linker_flags.
+    #
+    # Note: Rust's bootstrap system will use CFLAGS for both C and C++ compiler
+    #       invocations.
+    #
+    # Note: LTO is not enabled for device targets due to a bug either in the
+    #       ARMv7 implementation of compiler-rt or in LLD, which prevents the
+    #       resulting LTOed artifacts from linking properly.  See b/201551165.
+    env['HOST_CFLAGS'] = lto_flag
+
+    #
+    # Intantiate wrappers
+    #
 
     host_configs = '\n'.join(
         [host_config(target, macosx_flags, host_linker_flags) for target in HOST_TARGETS])
@@ -158,6 +226,8 @@ def configure():
     instantiate_template_file(
         CONFIG_TOML_TEMPLATE,
         OUT_PATH_RUST_SOURCE / 'config.toml',
+        llvm_cflags=lto_flag,
+        llvm_cxxflags=lto_flag,
         llvm_ldflags=host_linker_flags,
         all_targets=all_targets,
         cargo=CARGO_PATH,
