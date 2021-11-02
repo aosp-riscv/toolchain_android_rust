a884bfa84d354d906a4f95e9911bcd1933190ea2
Chris Wailes <chriswailes@google.com>
2021-11-02 11:11:59 -0700

    Add a script for generating Rust prebuilt update CLs
    This script performs the following actions:
     * Downloads prebuilt archives from the build server if they haven't
      been downloaded before
      * Extracts the archives into the appropriate directories
      * Updates the Android.bp build files
      * Commits the changes
    
    The script also supports generating prebuilt update CLs from locally
    built artifacts to aid in testing.
    
    Test: ./update_prebuilts.py 7853519 1.57.0 -i 203802952
    Change-Id: I9c2dfa08c512ab5288bbd36d59db4ff6caaf773c

--- paths.py
+++ paths.py
@@ -16,7 +16,7 @@
 from pathlib import Path
 import build_platform
 
-STAGE0_RUST_VERSION: str = '1.55.0'
+RUST_VERSION_STAGE0: str = '1.55.0'
 CLANG_REVISION:      str = 'r433403'
 CLANG_NAME:          str = 'clang-{0}'.format(CLANG_REVISION)
 GLIBC_VERSION:       str = '2.17-4.8'
@@ -34,20 +34,16 @@ OUT_PATH_PACKAGE:     Path = OUT_PATH / 'package'
 OUT_PATH_STDLIB_SRCS: Path = OUT_PATH_PACKAGE / 'src' / 'stdlibs'
 OUT_PATH_WRAPPERS:    Path = OUT_PATH / 'wrappers'
 
+DOWNLOADS_PATH: Path = WORKSPACE_PATH / '.downloads'
+
 LLVM_BUILD_PATH: Path = OUT_PATH_RUST_SOURCE / 'build' / build_platform.triple() / 'llvm' / 'build'
 
 PREBUILT_PATH:         Path = WORKSPACE_PATH / 'prebuilts'
-RUST_PREBUILT_PATH:    Path = PREBUILT_PATH / 'rust' / build_platform.prebuilt() / STAGE0_RUST_VERSION
+RUST_PREBUILT_PATH:    Path = PREBUILT_PATH / 'rust'
+RUST_HOST_STAGE0_PATH: Path = RUST_PREBUILT_PATH / build_platform.prebuilt() / RUST_VERSION_STAGE0
 LLVM_PREBUILT_PATH:    Path = PREBUILT_PATH / 'clang' / 'host' / build_platform.prebuilt() / CLANG_NAME
 LLVM_CXX_RUNTIME_PATH: Path = LLVM_PREBUILT_PATH / 'lib64'
 GCC_TOOLCHAIN_PATH:    Path = PREBUILT_PATH / 'gcc' / build_platform.prebuilt() / 'host' / ('x86_64-linux-glibc' + GLIBC_VERSION)
-
-# We live at      prebuilts/rust/${BUILD_PLATFORM}/${VERSION}/bin
-# libc++ lives at prebuilts/clang/host/${BUILD_PLATFORM}
-#                 /clang-${CLANG_REVISION}/lib64
-ANDROID_CXX_RUNTIME_PATH: Path = (
-    WORKSPACE_PATH / '..' / '..' / 'clang' / 'host' /
-        build_platform.prebuilt() / CLANG_NAME / 'lib64').resolve()
 
 PYTHON_PREBUILT_PATH:      Path = PREBUILT_PATH / 'python' / build_platform.prebuilt()
 CMAKE_PREBUILT_PATH:       Path = PREBUILT_PATH / 'cmake' / build_platform.prebuilt()
@@ -62,14 +58,20 @@ NDK_LLVM_PATH:    Path = NDK_PATH / 'toolchains' / 'llvm' / 'prebuilt' / 'linux-
 NDK_SYSROOT_PATH: Path = NDK_LLVM_PATH / 'sysroot'
 
 #
-# Paths to executables
+# Paths to toolchain executables
 #
 
-CARGO_PATH:  Path = RUST_PREBUILT_PATH   / 'bin' / 'cargo'
-RUSTC_PATH:  Path = RUST_PREBUILT_PATH   / 'bin' / 'rustc'
-PYTHON_PATH: Path = PYTHON_PREBUILT_PATH / 'bin' / 'python3'
-CC_PATH:     Path = LLVM_PREBUILT_PATH   / 'bin' / 'clang'
-CXX_PATH:    Path = LLVM_PREBUILT_PATH   / 'bin' / 'clang++'
-AR_PATH:     Path = LLVM_PREBUILT_PATH   / 'bin' / 'llvm-ar'
-RANLIB_PATH: Path = LLVM_PREBUILT_PATH   / 'bin' / 'llvm-ranlib'
-CXXSTD_PATH: Path = LLVM_PREBUILT_PATH   / 'include' / 'c++' / 'v1'
+CARGO_PATH:  Path = RUST_HOST_STAGE0_PATH / 'bin' / 'cargo'
+RUSTC_PATH:  Path = RUST_HOST_STAGE0_PATH / 'bin' / 'rustc'
+PYTHON_PATH: Path = PYTHON_PREBUILT_PATH  / 'bin' / 'python3'
+CC_PATH:     Path = LLVM_PREBUILT_PATH    / 'bin' / 'clang'
+CXX_PATH:    Path = LLVM_PREBUILT_PATH    / 'bin' / 'clang++'
+AR_PATH:     Path = LLVM_PREBUILT_PATH    / 'bin' / 'llvm-ar'
+RANLIB_PATH: Path = LLVM_PREBUILT_PATH    / 'bin' / 'llvm-ranlib'
+CXXSTD_PATH: Path = LLVM_PREBUILT_PATH    / 'include' / 'c++' / 'v1'
+
+#
+# Paths to binfs executables
+#
+
+FETCH_ARTIFACT_PATH: Path = Path('/google/data/ro/projects/android/fetch_artifact')
